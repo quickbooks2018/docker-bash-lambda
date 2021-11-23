0e2f294829830ae75e1c2d445f9f67e8c0713bb1
Muhammad Asim <asim@arbisoft.com>
2021-11-23 23:32:18 +0500

    Docker Bash Lambda

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,12 +13,12 @@ aws s3api create-bucket --bucket cloudgeeksca-lambda20 --region us-east-1
 
 aws s3api put-bucket-versioning --bucket cloudgeeksca-lambda20 --versioning-configuration Status=Enabled
 
-# ICMP not permitted
-ping -c3 cloudgeeks.ca
+# ICMP not permitted     # (this will trigger your script failed)
+# ping -c3 cloudgeeks.ca      
 
 
 # Stop Ec2
-aws ec2 stop-instances --instance-ids i-0d73c8f9824e39c22
+aws ec2 stop-instances --instance-ids i-0d88596481d3b9185
 
 
 
